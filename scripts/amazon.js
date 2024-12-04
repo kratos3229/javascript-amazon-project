ac29f5b35e4964dc6c9a1c1eca6251b2656856b2
Poovanna <poovannakeethianda@gmail.com>
2024-12-04 21:36:02 +0530

    Used products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -5,32 +5,7 @@ Main Idea of Javascript
 3. Make it interactive
 */
 
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
-
+// products array is being loaded from products.js
 let productsHTML = '';
 
 products.forEach((product, index) => {
