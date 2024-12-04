5f08ffd98a54f1ad0a4bf3e7fa4641c23446a79e
Poovanna <poovannakeethianda@gmail.com>
2024-12-04 22:15:46 +0530

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -13,23 +13,23 @@ products.forEach((product, index) => {
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
-          src="${product.image}">
+          src="${ product.image }">
       </div>
 
       <div class="product-name limit-text-to-2-lines">
-        ${product.name}
+        ${ product.name }
       </div>
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars * 10}.png">
+          src="images/ratings/rating-${ product.rating.stars * 10 }.png">
         <div class="product-rating-count link-primary">
-          ${product.rating.count}
+          ${ product.rating.count }
         </div>
       </div>
 
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${ (product.priceCents / 100).toFixed(2) }
       </div>
 
       <div class="product-quantity-container">
@@ -54,14 +54,37 @@ products.forEach((product, index) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${ product.id }">
         Add to Cart
       </button>
     </div>
   `;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach(button => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId; // Getting the product ID out of the element. data-product-id attribute in the button element stores the ID.
+      let matchingItem;
+
+      cart.forEach(item => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      // If item already exists, increase quantity by 1
+      if (matchingItem) {
+        matchingItem.quantity++;
+      } else {
+        cart.push({
+          productId,
+          quantity: 1
+        });
+      }
+      console.log(cart);
+    });
+  });
